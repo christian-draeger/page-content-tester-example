099c589d10a99466fb772122984c4361e0e43fdd
Christian Dräger <christian.draeger@idealo.de>
2017-04-13 17:14:09 +0200

    update example - working with v1.5.0

--- src/test/java/testcases/ExampleUsageTest.java
+++ src/test/java/testcases/ExampleUsageTest.java
@@ -1,50 +1,72 @@
 package testcases;
 
+import static fetcher.FetchedPage.fetchPage;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsMapContaining.hasEntry;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
+import annotations.FetchPage;
 import fetcher.FetchedPage;
+import runner.PageContentTester;
 
-public class ExampleUsageTest {
+public class ExampleUsageTest extends PageContentTester {
 
-    private final static String VALID_URL = "https://github.com/christian-draeger";
-    private static FetchedPage page;
-    private static FetchedPage pageAsMobileDevice;
+    private static final String GITHUB_URL = "https://github.com/christian-draeger";
+    private static final String GOOGLE_URL = "http://www.google.de";
+    private static final String VALID_SELECTOR = "h1";
 
-    @BeforeClass
-    public static void beforeAll() {
-        page = FetchedPage.fetchPage(VALID_URL);
-        pageAsMobileDevice = FetchedPage.fetchPageAsMobileDevice(VALID_URL);
+    @Test
+    @FetchPage(GITHUB_URL)
+    public void fetcher_should_return_count_of_certain_element() {
+        assertThat(page.get().getElementCount(VALID_SELECTOR), is(1));
     }
 
     @Test
-    public void is_correct_user_name() {
-        assertThat(page.getElement("h1").text(), containsString("christian-draeger"));
+    @FetchPage(GITHUB_URL)
+    @FetchPage(GOOGLE_URL)
+    public void fetch_multiple_pages_via_annotation_and_get_pages_by_url_snippet() {
+
+        FetchedPage github = page.get("github");
+
+        assertThat(github.isElementPresent("h1"), is(true));
+        assertThat(github.getUrl(), equalTo(GITHUB_URL));
+        assertThat(github.getElement("h1").text(), containsString("christian-draeger"));
+        assertThat(github.isElementPresent("img.avatar"), is(true));
+        assertThat(github.getElement("img.avatar").attr("src"), containsString("githubusercontent.com"));
+        assertThat(github.getCookieValue("logged_in"), equalTo("no"));
+        assertThat(github.getCookies(), hasEntry("logged_in", "no"));
+
+        FetchedPage google = page.get("google");
+        assertThat(google.isElementPresent("#footer"), is(true));
     }
 
     @Test
-    public void is_profile_picture_present() {
-        assertThat(page.isElementPresent("img.avatar"), is(true));
+    @FetchPage(GITHUB_URL)
+    @FetchPage(GOOGLE_URL)
+    public void fetch_multiple_pages_via_annotation_and_get_pages_by_index() {
+        FetchedPage github = page.get(0);
+        FetchedPage google = page.get(1);
+
+        assertThat(github.isElementPresent("h1"), is(true));
+
+        assertThat(google.isElementPresent("#footer"), is(true));
+        assertThat(google.getUrl(), equalTo(GOOGLE_URL));
     }
 
     @Test
-    public void check_profile_picture_src() {
-        assertThat(page.getElement("img.avatar").attr("src"), containsString("githubusercontent.com"));
+    public void fetch_multiple_pages_in_test_method() {
+        FetchedPage github = fetchPage(GITHUB_URL);
+        FetchedPage google = fetchPage(GOOGLE_URL);
+
+        assertThat(github.isElementPresent("h1"), is(true));
+        assertThat(github.getUrl(), equalTo(GITHUB_URL));
+
+        assertThat(google.isElementPresent("#footer"), is(true));
+        assertThat(google.getUrl(), equalTo(GOOGLE_URL));
     }
 
-    @Test
-    public void check_login_cookie_status() {
-        assertThat(page.getCookieValue("logged_in"), equalTo("no"));
-    }
-
-    @Test
-    public void fetcher_should_return_cookies() {
-        assertThat(page.getCookies(), hasEntry("logged_in", "no"));
-    }
 }
